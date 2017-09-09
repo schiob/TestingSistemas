6d7784bebfe232d56f0df358d5776d29a4b0f023
fanborges <fanborges@gmail.com>
2017-09-08 20:33:27 -0500

    Separación de los test unitarios en archivos individuales

--- ago-dic-2017/Luis Clemente Zúñiga Flores/Práctica 1/TestUnitario.py
+++ ago-dic-2017/Luis Clemente Zúñiga Flores/Práctica 1/TestUnitario.py
@@ -1,16 +1,14 @@
 import unittest
-from suma import suma_nums
 from candles import birthdayCakeCandles
-from timeconversion import timeConversion
 
+
+#Test unitario para la clase birthdayCakeCandles
 class TestUnitario(unittest.TestCase):
    
    def testCandles(self):
-	   arr = [10,11,12,13,14]
-	   self.assertEqual(suma_nums(arr),'46 50')
+	   arr = [10,11,12,13,14]	   
 	   self.assertEqual(birthdayCakeCandles(5,arr),1)
-	   self.assertEqual(timeConversion('07:05:45PM'),'19:05:45')
-	   
+	 
 if __name__ == '__main__':
     unittest.main()
 
