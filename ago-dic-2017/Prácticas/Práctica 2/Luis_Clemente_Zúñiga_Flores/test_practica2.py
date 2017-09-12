5708957b8b3679873b1c2788c8d989fe8bf19421
fanborges <fanborges@gmail.com>
2017-09-12 17:53:18 -0500

    se agrega el método setup

--- ago-dic-2017/Prácticas/Práctica 2/Luis_Clemente_Zúñiga_Flores/test_practica2.py
+++ ago-dic-2017/Prácticas/Práctica 2/Luis_Clemente_Zúñiga_Flores/test_practica2.py
@@ -7,41 +7,46 @@ from practica2 import writeFile
 
 class testPractica2(unittest.TestCase):
     
-    #archivo para probar string 'hola tacos'	
-	taco = open('./holatacos.txt','w')
-	taco.write('hola tacos')
-	taco.close()
-	
-    #archivo para probar contenido vacío
-	vacio = open('vacio.txt','w')
-	vacio.write('')
-	vacio.close()
-	
-    #archivo para probar "hola\nsalto\ntamal"
-	salto = open('salto.txt','w')
-	salto.write('hola\nsalto\ntamal')
-	salto.close()
-	
-    #archivo para probar path nuevo.
-	nuevo = open('./nuevo.txt','w')
-	nuevo.write('Este es un texto')
-	nuevo.close()	
-		
-	def testHolaTacos(self):
-		self.assertEqual(printFile(self.taco.name),"hola tacos")
-		
-	def testVacio(self):
-		self.assertEqual(printFile(self.vacio.name),'')
-	
-	def testSalto(self):
-		self.assertEqual(printFile(self.salto.name),'hola\nsalto\ntamal')
-	
-	def testSobreEscrituraArchivoNuevo(self):		
-		self.assertEqual(writeFile(self.nuevo.name,'Charmander'),'Charmander')
-	 	
-	def testSobreEscrituraArchivoExistente(self):
-		self.assertEqual(writeFile(self.nuevo.name,'Ya es viernes'),'Ya es viernes')
-		
-		
+    
+    def setUp(self):        
+        #archivo para probar string 'hola tacos'    
+        self.taco = open('./holatacos.txt','w')
+        self.taco.write('hola tacos')
+        self.taco.close()
+        
+        #archivo para probar contenido vacío
+        self.vacio = open('vacio.txt','w')
+        self.vacio.write('')
+        self.vacio.close()
+        
+        #archivo para probar "hola\nsalto\ntamal"
+        self.salto = open('salto.txt','w')
+        self.salto.write('hola\nsalto\ntamal')
+        self.salto.close()
+        
+        #archivo para probar path nuevo.
+        self.nuevo = open('./nuevo.txt','w')
+        self.nuevo.write('Este es un texto')
+        self.nuevo.close()   
+        
+        
+    def testHolaTacos(self):
+        self.assertEqual(printFile(self.taco.name),"hola tacos")
+        
+    def testVacio(self):
+        self.assertEqual(printFile(self.vacio.name),'')
+    
+    def testSalto(self):
+        self.assertEqual(printFile(self.salto.name),'hola\nsalto\ntamal')
+    
+    def testSobreEscrituraArchivoNuevo(self):       
+        self.assertEqual(writeFile(self.nuevo.name,'Charmander'),'Charmander')
+        
+    def testSobreEscrituraArchivoExistente(self):
+        self.assertEqual(writeFile(self.nuevo.name,'Ya es viernes'),'Ya es viernes')
+    
+    
+        
+        
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
