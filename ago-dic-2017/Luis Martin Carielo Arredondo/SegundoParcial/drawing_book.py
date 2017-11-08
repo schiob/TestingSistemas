3d6f78bded1d6122e79f3bc11c56af4bb645d388
luca83 <lcarielo@hotmail.com>
2017-11-08 11:19:20 -0600

    Subiendo carpeta de examen SegundoParcial

---
+++ ago-dic-2017/Luis Martin Carielo Arredondo/SegundoParcial/drawing_book.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import sys
+
+def solve(n, p):
+    if p <= n/2:
+        return p//2
+    else:
+        return n//2 - p//2
+
+if __name__ == "__main__":
+	n = int(input().strip())
+	p = int(input().strip())
+	result = solve(n, p)
+	print(result)
