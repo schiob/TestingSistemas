95a15ec6922802f918d4f36de4d8163484bf4d51
Juan Carlos Sleiman <jcsleiman17@gmail.com>
2018-01-22 19:45:39 -0600

    Practica conteo de numeros en Python

---
+++ ene-jun-2018/Juan Carlos Sleiman/Practica 1/hola.py
@@ -0,0 +1,58 @@
+pos = 0
+neg = 0
+par = 0
+imp = 0
+a=0
+b=0
+c=0
+d=0
+e=0
+a,b,c,d,e = input('Pon 5 numeros').split(' ')
+		
+if int(str(a)) > 0:
+	pos = pos + 1
+elif int(str(a)) < 0:
+		neg = neg + 1
+if int(str(b)) > 0:
+	pos = pos + 1
+elif int(str(b)) < 0:
+		neg = neg + 1
+if int(str(c)) > 0:
+	pos = pos + 1
+elif int(str(c)) < 0:
+		neg = neg + 1
+if int(str(d)) > 0:
+	pos = pos + 1
+elif int(str(d)) < 0:
+		neg = neg + 1
+if int(str(e)) > 0:
+	pos = pos + 1
+elif int(str(e)) < 0:
+		neg = neg + 1
+
+	
+if int(str(a))%2 == 0:
+	par = par+1
+else:
+	imp = imp + 1
+if int(str(b))%2 == 0:
+	par = par+1
+else:
+	imp = imp + 1
+if int(str(c))%2 == 0:
+	par = par+1
+else:
+	imp = imp + 1
+if int(str(d))%2 == 0:
+	par = par+1
+else:
+	imp = imp + 1
+if int(str(e))%2 == 0:
+	par = par+1
+else:
+	imp = imp + 1
+
+print (str(pos) + " número(s) positivo(s)" )
+print (str(neg) + " número(s) negativos(s)" )
+print (str(par) + " número(s) par(es)" )
+print (str(imp) + " número(s) impar(es)" )
