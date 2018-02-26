d217b4b30abb10ab91ceb58ea89e7792b25714ec
AnaCazares <ana_cdl18@hotmail.com>
2018-02-25 20:35:39 -0600

    Practica 3 modificada

--- ene-jun-2018/AnaCazares/Practica3/TestTriangulo.py
+++ ene-jun-2018/AnaCazares/Practica3/TestTriangulo.py
@@ -1,24 +1,22 @@
 import unittest
-from triangulo import lado
+from triangulo import Triangulo
 
 class TestUnitario(unittest.TestCase):
 	#Función "constructor":
-	def setUp(self):
-		self.lista=[1,2,3]
-
+        def setUp(self):
 	#Funciones test:
-    def test_equilatero(self):
-        result = lista(3, 3, 3)
-        self.assertEqual(result, 'Equilatero')
-    def test_isosceles(self):
-        result = lista(3, 3, 2)
-        self.assertEqual(result, 'Isósceles')
-    def test_escaleno(self):
-        result = lista(2, 3, 4)
-        self.assertEqual(result, 'Escaleno')
-    def test_false(self):
-        result = lista(1, 2, 3)
-        self.assertEqual(result, 'No es rectangulo')
+                def test_equilatero(self):
+                        result = lista(3, 3, 3)
+                self.assertEqual(result, 'Equilatero')
+                def test_isosceles(self):
+                        result = lista(3, 3, 2)
+                self.assertEqual(result, 'Isósceles')
+                def test_escaleno(self):
+                        result = lista(2, 3, 4)
+                self.assertEqual(result, 'Escaleno')
+                def test_false(self):
+                        result = lista(1, 2, 3)
+                self.assertEqual(result, 'No es rectangulo')
 		
 if __name__ == '__main__':
 	unittest.main()
