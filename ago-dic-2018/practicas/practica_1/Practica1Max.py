8905c28c05dac97fe6043e3729c5857b9eb87318
Max <38051426+RaizenBS7@users.noreply.github.com>
2018-08-31 01:42:28 -0500

    Practica 1 Max Mejorada
    Realice unos cambios que solucionaron algunos errores,
    ademas que que ahora esta mejor acomodado la salida de los datos.

--- ago-dic-2018/practicas/practica_1/Practica1Max.py
+++ ago-dic-2018/practicas/practica_1/Practica1Max.py
@@ -1,17 +1,17 @@
 import math
-N= int(input("Numeros que deseas clasificar: "))
-Pos= 0
-Neg= 0
-Par= 0
-Impar= 0
-lista= [] #cree esta lista vacia para guardar los valores random y poder ver lo que me dio mas tarde
-for n in (0, N):
-    x = int(input('Dame un valor: ')))
+N=int(input("Cantidad de numeros que deseas clasificar: "))
+Pos = 0
+Neg = 0
+Par = 0
+Impar = 0
+lista = [] #cree esta lista vacia para guardar los valores y poder ver mas tarde
+for n in range(N):
+    x= int(input("Dame un numero" , n, " :" ))
     lista.append(x)
-    if x>= 0:
+    if x >= 0:
         Pos= Pos + 1
-        if (x % 2) == 0:
-            Par= Par + 1 
+        if (x%2)== 0:
+            Par= Par + 1
         else:
             Impar= Impar + 1
     else:
@@ -20,8 +20,8 @@ for n in (0, N):
             Par= Par + 1
         else:
             Impar= Impar + 1
-print("Numeros positivos: ",  Pos)
-print("Numeros Negativos: ",  Neg)
-print("Numeros Pares: ",  Par)
-print("Numeros Impares: ",  Impar)
-print(lista)
+print("Numero(s) positivo(s) : ",  Pos)
+print("Numero(s) Negativo(s) : ",  Neg)
+print("Numero(s) Pare(s)     : ",  Par)
+print("Numero(s) Impare(s)   : ",  Impar)
+print("Numeros ingresados: ", lista)
