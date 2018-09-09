ab6b66315cca0fe5aeefdcd2dabf66c0a5bf1093
Cynthia\Cynthia <cgg2709@gmail.com>
2018-09-09 16:56:52 -0500

    añadiendo archivos modificados practica 3

--- ago-dic-2018/CynthiaGarcía/numTriTest.py
+++ ago-dic-2018/CynthiaGarcía/numTriTest.py
@@ -3,8 +3,21 @@ import numerosTriangulares
 
 class  TestTriangulares(unittest.TestCase):
 		def test_triang(self):
-			resul=numerosTriangulares.calcNT(2)
-			self.assertEqual(resul(trian),2)
+			re=numerosTriangulares.calcNT(5)
+			re2=numerosTriangulares.calcNT(1)
+			re3=numerosTriangulares.calcNT(3)
+			re4=numerosTriangulares.calcNT(4)
+			re5=numerosTriangulares.calcNT(56)
+			re6=numerosTriangulares.calcNT(40)
 
-if __name__ == '__name__'	:
-	unittest.main	
+			self.assertEqual(re, 15)
+			self.assertEqual(re2, 1)
+			self.assertEqual(re3, 6)
+			self.assertEqual(re4, 10)
+			self.assertEqual(re5, 1596)
+			self.assertEqual(re6, 80200)
+
+if __name__ == '__main__' :	
+	unittest.main()
+
+	
