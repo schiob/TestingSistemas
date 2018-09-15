ccd2a8632533b167050586af24242ff2793d16e1
AngieRdz <angela.aracelyr@gmail.com>
2018-09-15 00:30:49 -0500

    mofificacion Pactica 2

--- ago-dic-2018/AngelaRodriguez/Practica2/cpractica_test.py
+++ ago-dic-2018/AngelaRodriguez/Practica2/cpractica_test.py
@@ -4,11 +4,30 @@ import cpractica
 class TestJuanita (unittest.TestCase):
 
 	def test_numeros(self):
-		res=cpractica.calc([0,3,42,5,6])
-		self.assertEqual(res[0], 3)
-		self.assertEqual(res[1], 2)
-		self.assertEqual(res[2], 0)
-		self.assertEqual(res[3], 4)
+		res1=cpractica.calc([0,3,42,5,6])
+		self.assertEqual(res1[0], 3)
+		self.assertEqual(res1[1], 2)
+		self.assertEqual(res1[2], 0)
+		self.assertEqual(res1[3], 4)
+
+		res2=cpractica.calc([10,4,-4,6,-2])
+		self.assertEqual(res2[0], 5)
+		self.assertEqual(res2[1], 0)
+		self.assertEqual(res2[2], 2)
+		self.assertEqual(res2[3], 3)
+
+		res3=cpractica.calc([-1,25,90,13,-2])
+		self.assertEqual(res3[0], 2)
+		self.assertEqual(res3[1], 3)
+		self.assertEqual(res3[2], 2)
+		self.assertEqual(res3[3], 3)
+
+		res4=cpractica.calc([-17,3,0,14,5])
+		self.assertEqual(res4[0], 2)
+		self.assertEqual(res4[1], 3)
+		self.assertEqual(res4[2], 1)
+		self.assertEqual(res4[3], 3)
+
 
 if __name__ =='__main__':
 	unittest.main() 
