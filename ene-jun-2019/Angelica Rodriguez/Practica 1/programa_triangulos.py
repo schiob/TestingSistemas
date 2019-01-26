65eadb961ba77bfa7327436b55028d417b4a785a
angelicardz <angelica_marc@hotmail.com>
2019-01-26 00:38:39 -0600

    Primer Commit

---
+++ ene-jun-2019/Angelica Rodriguez/Practica 1/programa_triangulos.py
@@ -0,0 +1,18 @@
+"""
+Programa que lee tres números enteros y a partir de ellos,
+define si éstos forman un triángulo, en caso de ser así,
+indica el tipo de triángulo según sus lados.
+"""
+# Leer tres números enteros
+num1, num2, num3 = list(map(int, input().strip().split()))
+
+# No se puede formar un triángulo si la suma de dos de sus lados es
+# menor o igual al tercer lado
+if num1 >= (num2 + num3) or num2 >= (num1 + num3) or num3 >= (num1 + num2):
+    print("No triángulo")
+elif num1 == num2 == num3:    # tres lados iguales
+    print("Equilátero")
+elif num1 == num2 or num2 == num3 or num1 == num3:    # dos lados iguales
+    print("Isósceles")
+else:
+    print("Escaleno")   # si no es isosceles, ni equilátero, es escaleno :D
