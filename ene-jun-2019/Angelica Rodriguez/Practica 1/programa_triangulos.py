77481a41ea1c8872673749001f2572c12290af73
angelicardz <angelica_marc@hotmail.com>
2019-02-06 04:49:09 -0600

    modificaciones entradas

--- ene-jun-2019/Angelica Rodriguez/Practica 1/programa_triangulos.py
+++ ene-jun-2019/Angelica Rodriguez/Practica 1/programa_triangulos.py
@@ -4,15 +4,24 @@ define si éstos forman un triángulo, en caso de ser así,
 indica el tipo de triángulo según sus lados.
 """
 # Leer tres números enteros
-num1, num2, num3 = list(map(int, input().strip().split()))
+#num1, num2, num3 = list(map(int, input().strip().split()))
 
-# No se puede formar un triángulo si la suma de dos de sus lados es
-# menor o igual al tercer lado
-if num1 >= (num2 + num3) or num2 >= (num1 + num3) or num3 >= (num1 + num2):
-    print("No triángulo")
-elif num1 == num2 == num3:    # tres lados iguales
-    print("Equilátero")
-elif num1 == num2 or num2 == num3 or num1 == num3:    # dos lados iguales
-    print("Isósceles")
-else:
-    print("Escaleno")   # si no es isosceles, ni equilátero, es escaleno :D
+def ladosTriangulo(num1, num2, num3):
+    try:
+        # No se puede formar un triángulo si la suma de dos de sus lados es
+        # menor o igual al tercer lado
+
+        if num1 >= (num2 + num3) or num2 >= (num1 + num3) or num3 >= (num1 + num2):
+            return "No triángulo"
+        elif num1 == num2 == num3:    # tres lados iguales
+            return "Equilátero"
+        elif num1 == num2 or num2 == num3 or num1 == num3:    # dos lados iguales
+            return "Isósceles"
+        else:
+            return "Escaleno"   # si no es isosceles, ni equilátero, es escaleno :D
+    except:
+        return "Entrada incorrecta"
+
+if __name__ == '__main__':
+    num1, num2, num3 = list(map(int, input().strip().split()))
+    print(ladosTriangulo(num1,num2,num3))
