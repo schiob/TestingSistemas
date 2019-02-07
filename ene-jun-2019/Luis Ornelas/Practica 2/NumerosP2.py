ff835a1e1d5c0209f0721d4781344eae5afc0850
Luis <luis_al_or@hotmail.com>
2019-02-06 18:42:08 -0600

    Test Actualizado

--- ene-jun-2019/Luis Ornelas/Practica 2/NumerosP2.py
+++ ene-jun-2019/Luis Ornelas/Practica 2/NumerosP2.py
@@ -1,6 +1,6 @@
-def Calcular(numero):
+def Calcular(numeros):
     par = impar = negativo = positivo = 0
-    for num in numero:
+    for num in numeros:
         if num % 2 == 0:
             par = par + 1
         else:
@@ -9,12 +9,12 @@ def Calcular(numero):
             positivo += 1
         elif num < 0:
             negativo += 1
-    return (f"{positivo} Numero(s) Postivo(s)\n{negativo} Numeros Negativo(s)\n{par} Numero(s) Par(es)\n{impar} Numeors Impar(es)")
+    return "{} Numero(s) Positivo(s)\n{} Numero(s) Negativo(s)\n{} Numero(s) Par(es)\n{} Numero(s) Impar(es)".format(positivo,negativo,par,impar)
 
 if __name__ == '__main__':
     print("Introduce Total de Numeros: ")
     num = input()
-    print("Introduce los Numeros(Separados por un Espacio): ")
+    print("Introduce Los Numeros(Separados por un Espacio): ")
     numeros = input()
     numeros = list(map(int, numeros.split()))
     print(type((numeros)))
