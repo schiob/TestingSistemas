d8a528f9d0b8aef8de55c0ab5d5bef51ad7c1793
angelicardz <angelica_marc@hotmail.com>
2019-02-13 19:13:25 -0600

    primer parcial

--- ene-jun-2019/Angelica Rodriguez/Parcial 1/hoursToHours.py
+++ ene-jun-2019/Angelica Rodriguez/Parcial 1/hoursToHours.py
@@ -10,14 +10,56 @@ def convertHours(stringHora):
         horas = stringHora.split(':')  # cortamos el string
         minutos = horas[1].split('hrs')  # cortamos el string
         horas = int(horas[0])  # obtenemos la hora
-        minutos = int(minutos[0])  # obtenemos los minutos
-
+        minutos = int(minutos[0])
+        h = 0
+        m = 0
+        masdoce = 0
+        if horas == 0:
+            horas = "00"
+            h = 1
+        if minutos == 0:
+            minutos = "00"
+            m = 1
         if horas > 12:
-            horas = horas-12   # convertimos la hora a formato de 12 hrs
-            return """0{}:{}p.m.""".format(horas, minutos).strip()
-        elif horas < 12:
-            return """0{}:{}a.m.""".format(horas, minutos).strip()
-        else:  # cuando la hora sea 12
-            return """{}:{}p.m.""".format(horas, minutos).strip()
+            horas = horas-12
+            masdoce = 1
+        #print(horas)
+
+        if m == 1:
+            if masdoce == 1:
+                if horas < 12 and horas < 10:
+                    return """0{}:{}p.m.""".format(horas, minutos).strip()
+                if horas >9 and horas < 12:
+                    return """{}:{}p.m.""".format(horas, minutos).strip()
+            else:
+                if horas < 10:
+                    return """0{}:{}p.m.""".format(horas, minutos).strip()
+                if horas > 9:
+                    return """{}:{}p.m.""".format(horas, minutos).strip()
+        else:
+            if masdoce == 1:
+                if horas < 12 and horas < 10 and minutos >10:
+                    return """0{}:{}p.m.""".format(horas, minutos).strip()
+                if horas < 12 and horas < 10 and minutos < 10:
+                    return """0{}:0{}p.m.""".format(horas, minutos).strip()
+                if horas >9 and horas < 12 and minutos > 10:
+                    return """{}:{}p.m.""".format(horas, minutos).strip()
+                if horas >9 and horas < 12 and minutos < 10:
+                    return """{}:0{}p.m.""".format(horas, minutos).strip()
+
+            else:
+                if horas < 10 and minutos > 10:
+                    return """0{}:{}a.m.""".format(horas, minutos).strip()
+                if horas < 10 and minutos < 10:
+                        return """0{}:0{}a.m.""".format(horas, minutos).strip()
+                if horas > 9 and horas < 12 and minutos > 10:
+                    return """{}:{}a.m.""".format(horas, minutos).strip()
+                if horas > 9 and horas < 12 and minutos < 10:
+                    return """{}:0{}a.m.""".format(horas, minutos).strip()
+
+
     except:
         return "Entrada incorrecta"
+
+if __name__ == '__main__':
+    print(convertHours('00:00hrs'))
