1f16acd1347afcb9e9a1028769bda8d29539e511
DavidSena <angelsenamartinez@uadec.edu.mx>
2020-01-24 22:00:48 -0600

    Practica1

---
+++ ene-jun-2020/David Sena/Practica 1/Cilindro.py
@@ -0,0 +1,12 @@
+#Calcular el volumen de un cilindro
+import math 
+def VolCilindro(radio,altura):
+    volumen = math.pi*radio**2*altura
+    
+  
+    
+    return volumen
+
+print(VolCilindro(12,15))
+
+
