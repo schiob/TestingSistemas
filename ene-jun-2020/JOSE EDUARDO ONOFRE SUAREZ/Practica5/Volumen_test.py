6cd8351b08862d7e6b991f423500b1d5ce051f16
unknown <onofreeduardos@gmail.com>
2020-05-08 20:25:19 -0500

    Se modifico los Unitest

--- ene-jun-2020/JOSE EDUARDO ONOFRE SUAREZ/Practica5/Volumen_test.py
+++ ene-jun-2020/JOSE EDUARDO ONOFRE SUAREZ/Practica5/Volumen_test.py
@@ -1,5 +1,53 @@
 import unittest
 from Volumen import volumen
 
+
 class TestVolumen(unittest.TestCase):
-    
+    def test_calVol(self):
+        testcases = [
+
+            {
+                "nombre" : "calcular volumen",
+                "entradas": [
+                        
+                    {"largo": 2},
+                    {"ancho": 3},
+                    {"alto": 4},
+
+                ],
+                "salida_esperada": 24
+            },
+            {
+                "nombre" : "calcular volumen 2",
+                "entradas": [
+                        
+                    {"largo": 5},
+                    {"ancho": 2},
+                    {"alto": 3},
+
+                ],
+                "salida_esperada": 30
+            },
+            {
+                "nombre" : "calcular volumen con numero negativo",
+                "entradas": [
+                        
+                    {"largo": -5},
+                    {"ancho": 2},
+                    {"alto": 3},
+
+                ],
+                "salida_esperada": 25
+            }
+
+
+        ]
+
+        for tc in testcases:
+            vol = volumen.calVol(tc["entradas"][0]["largo"],tc["entradas"][1]["ancho"],tc["entradas"][2]["alto"])
+
+            self.assertEqual(vol,tc["salida_esperada"], msg = "Error dato invalido")
+
+
+if __name__ == "__main__":
+    unittest.main()
