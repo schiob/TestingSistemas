263b529f05539b61652523fbf8d94816e640d368
unknown <onofreeduardos@gmail.com>
2020-05-10 02:06:53 -0500

    Se modifico y agregaron mas pruebas unitarias

--- ene-jun-2020/JOSE EDUARDO ONOFRE SUAREZ/Practica5/Volumen_test.py
+++ ene-jun-2020/JOSE EDUARDO ONOFRE SUAREZ/Practica5/Volumen_test.py
@@ -1,5 +1,7 @@
 import unittest
 from Volumen import volumen
+from Contenedor import contenedor
+
 
 
 class TestVolumen(unittest.TestCase):
@@ -37,7 +39,7 @@ class TestVolumen(unittest.TestCase):
                     {"alto": 3},
 
                 ],
-                "salida_esperada": 25
+                "salida_esperada": 30
             }
 
 
@@ -47,6 +49,64 @@ class TestVolumen(unittest.TestCase):
             vol = volumen.calVol(tc["entradas"][0]["largo"],tc["entradas"][1]["ancho"],tc["entradas"][2]["alto"])
 
             self.assertEqual(vol,tc["salida_esperada"], msg = "Error dato invalido")
+
+
+
+    def test_volumencontenedor(self):
+        testcases = [
+
+            {
+                "nombre" : "calcular volumen correcto",
+                "entradas": [
+                        
+                    {"contenedores": [5,2,6]},
+                    {"volumenes": [4,3,2]},
+                    
+
+                ],
+                "salida_esperada": 38
+            },
+            {
+                "nombre" : "calcular volumen incorrecto",
+                "entradas": [
+                        
+                    {"contenedores": [5,2,6]},
+                    {"volumenes": [4,3,2]},
+                    
+
+                ],
+                "salida_esperada": 36
+            },
+            {
+                "nombre" : "calcular volumen con un 0",
+                "entradas": [
+                        
+                    {"contenedores": [0,0,0]},
+                    {"volumenes": [4,3,2]},
+                    
+
+                ],
+                "salida_esperada": 0
+            },
+            {
+                "nombre" : "calcular volumen con 0 dando valor positivo",
+                "entradas": [
+                        
+                    {"contenedores": [5,2,6]},
+                    {"volumenes": [0,0,0]},
+                    
+
+                ],
+                "salida_esperada": 38
+            }
+
+
+        ]
+
+        for tc in testcases:
+            volu = contenedor.volumencontenedor(tc["entradas"][0]["contenedores"],tc["entradas"][1]["volumenes"])
+
+            self.assertEqual(volu,tc["salida_esperada"], msg = "Error dato invalido")
 
 
 if __name__ == "__main__":
