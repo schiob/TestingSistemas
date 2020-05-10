52803712b7e252ed50611ac4bdf41fd67b76a236
EmilioBG1997 <emilio.barrera1997@gmail.com>
2020-05-09 19:46:25 -0500

    added practice #5

---
+++ ene-jun-2020/EmilioBarreraGonzalez/practica 5/contenedores.py
@@ -0,0 +1,37 @@
+import math
+#Function to calculate the volume 
+#Of a cubic container given it's 
+#3 sides
+def vol_calc(side1: float, side2: float, side3: float) -> float: 
+    try:
+       vol = float(side1 * side2 * side3)
+       return vol
+    except:
+        print("Numeric values for each of the 3 sides were expected")
+        return -1 #ERROR CODE
+
+def total_vol(containers: list) -> float: 
+    rvalue=0
+    for i in containers:
+        a=i.split(',')
+        rvalue+=float(a[0])*float(a[1])
+    return rvalue
+
+def saving_strat(side: list,vol: float, volume_calc: object) -> list: 
+    rvalue=[]
+    side.sort(reverse=True)
+    for i in side:
+        if(i==side[len(side)-1]):
+            tot = math.ceil(vol/volume_calc(i,i,i))
+        elif(volume_calc(i,i,i) >= vol):
+            rvalue.append(f"({i},1)")
+            return rvalue
+        else:
+            tot = math.floor(vol/volume_calc(i,i,i))
+            vol %= volume_calc(i,i,i)
+        rvalue.append(f"({i},{tot})")
+    return rvalue
+
+if __name__ == '__main__':
+    side=[1,2,3]
+    print(saving_strat(side, 200, vol_calc))
---
+++ ene-jun-2020/EmilioBarreraGonzalez/practica 5/contenedores_test.py
@@ -0,0 +1,106 @@
+import unittest
+from contenedores import *
+def vol_calcmock(input1,input2,input3):
+    return input1*input2*input3
+
+class test_contenedores(unittest.TestCase):
+    def test_volCalc(self):
+        test_cases = [
+            {
+                'name': 'PRUEBA DE CONTENEDORES  3 x 3 x 3',
+                'input': ([3.0,3.0,3.0]),
+                'expected_out': 27.0
+            },
+            {
+                'name': 'PRUEBA DE CONTENEDORES  4 x 4 x 4',
+                'input': ([4.0,4.0,4.0]),
+                'expected_out': 64.0
+            },
+            {
+                'name': 'PRUEBA DE CONTENEDORES  5 x 5 x 5',
+                'input': ([5.0,5.0,5.0]),
+                'expected_out': 125.0
+            },
+            {
+                'name': 'PRUEBA DE CONTENEDORES  2 x 2 x 2',
+                'input': ([2.0,2.0,2.0]),
+                'expected_out': 8.0
+            },
+            {
+                'name': 'PRUEBA DE CONTENEDORES  1 x 1 x 1',
+                'input': [1.0,1.0,1.0],
+                'expected_out': 1.0
+            }
+        ]
+        for i in test_cases:
+            actual=vol_calc(i['input'][0],i['input'][1],i['input'][2])
+            self.assertEqual(actual,i['expected_out'])
+    def test_totalVol(self):
+        test_cases = [
+            {
+                'name': 'PRUEBA 3 contenedores de Volumen 1, 2 de 4 y 5 de 3',
+                'input': ['3,1','2,4','5,3'],
+                'expected_out':26.0
+            },
+            {
+                'name': 'PRUEBA 4 contenedores de Volumen 2, 5 de 1 y 5 de 6',
+                'input': ['4,2','5,1','5,6'],
+                'expected_out':43.0
+            },
+            {
+                'name': 'PRUEBA 2 contenedores de Volumen 2, 4 de 4 y 5 de 5',
+                'input': ['2,2','4,4','5,5'],
+                'expected_out':45.0
+            }
+        ]
+        for i in test_cases:
+            actual = total_vol(i['input'])
+            self.assertEqual(actual,i['expected_out'])
+
+    def test_savingstratUnit(self): #USANDO UN MOCK
+        test_cases = [
+            {
+                'name': 'cajas de 1, 2 y 3 para un volumen de 100',
+                'input': [[1,2,3],100],
+                'expected':['(3,3)','(2,2)','(1,3)']
+            },
+            {
+                'name': 'cajas de 2, 4 y 6 para un volumen de 500',
+                'input': [[2,4,6],500],
+                'expected':['(6,2)','(4,1)','(2,1)']
+            },
+            {
+                'name': 'cajas de 1, 2 y 3 para un volumen de 200',
+                'input': [[1,2,3],200],
+                'expected':['(3,7)','(2,1)','(1,3)']
+            }
+        ]
+        for i in test_cases:
+            actual = saving_strat(i['input'][0],i['input'][1],vol_calcmock)
+            self.assertEqual(actual,i['expected'])
+    
+    def test_savingstrat(self): ##INTEGRANDO AMBAS FUNCIONES
+        test_cases = [
+            {
+                'name': 'cajas de 1, 2 y 3 para un volumen de 100',
+                'input': [[1,2,3],100],
+                'expected':['(3,3)','(2,2)','(1,3)']
+            },
+            {
+                'name': 'cajas de 2, 4 y 6 para un volumen de 500',
+                'input': [[2,4,6],500],
+                'expected':['(6,2)','(4,1)','(2,1)']
+            },
+            {
+                'name': 'cajas de 1, 2 y 3 para un volumen de 200',
+                'input': [[1,2,3],200],
+                'expected':['(3,7)','(2,1)','(1,3)']
+            }
+        ]
+        for i in test_cases:
+            actual = saving_strat(i['input'][0],i['input'][1],vol_calc)
+            self.assertEqual(actual,i['expected'])
+
+
+if __name__ == '__main__':
+    unittest.main()
