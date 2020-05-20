413cd3a5bdd6447c0ca899f9a8b3bd36267f7a6d
unknown <onofreeduardos@gmail.com>
2020-05-20 00:18:34 -0500

    UPDATE Importante

---
+++ ene-jun-2020/JOSE EDUARDO ONOFRE SUAREZ/ProyectoFinalYGODeckMaker/ProyectoFinalYGO/DBDecks.py
@@ -0,0 +1,71 @@
+    ## Crear Tabla MainDeck ##
+    ## Insertar en MainDeck ##
+
+    ##AQUI VAN LOS METODOS PARA CREAR LA TABLA DEL MAIN DECK
+    ##EL METODO PARA INSERTAR EN EL MAINDECK
+    ##EL METODO PARA VISUALIZAR LAS CARTAS DEL MAINDECK
+    #NOTAAAAAA: RECORDAR QUE CADA FUNCION TIENE SU INTERFAZ PARA PODER HACER UN MOCK DE PRUEBAS
+    def insertarMain():
+        pass #AQUI DEBEMOS PONER EL METODO YA HECHO EN EL ARCHIVO "DataBaseYGO.py"
+
+
+    
+    def VisualizarMain():
+        pass #AQUI DEBEMOS PONER EL METODO YA HECHO EN EL ARCHIVO "DataBaseYGO.py"
+            #DEBE DE RETORNAR UNA LISTA PARA PASAR AL OBJETO DECK
+    
+    #########################
+
+
+    ## Crear Tabla ExtraDeck ##
+    ## Insertar en ExtraDeck ##
+
+    ##AQUI VAN LOS METODOS PARA CREAR LA TABLA DEL EXTRA DECK
+    ##EL METODO PARA INSERTAR EN EL EXTRADECK
+    ##EL METODO PARA VISUALIZAR LAS CARTAS DEL EXTRADECK
+    #NOTAAAAAA: RECORDAR QUE CADA FUNCION TIENE SU INTERFAZ PARA PODER HACER UN MOCK DE PRUEBAS
+    def insertarExtra():
+        pass #AQUI DEBEMOS PONER EL METODO YA HECHO EN EL ARCHIVO "DataBaseYGO.py"
+        
+
+    
+    def VisualizarExtra():
+        pass #AQUI DEBEMOS PONER EL METODO YA HECHO EN EL ARCHIVO "DataBaseYGO.py"
+                #DEBE DE RETORNAR UNA LISTA PARA PASAR AL OBJETO DECK
+    
+    
+    #########################
+
+
+
+
+
+    #########################
+
+
+    ## Crear Tabla SideDeck ##
+    ## Insertar en SideDeck ##
+
+    ##AQUI VAN LOS METODOS PARA CREAR LA TABLA DEL MAIN DECK
+    ##EL METODO PARA INSERTAR EN EL MAINDECK
+    ##EL METODO PARA VISUALIZAR LAS CARTAS DEL MAINDECK
+    #NOTAAAAAA: RECORDAR QUE CADA FUNCION TIENE SU INTERFAZ PARA PODER HACER UN MOCK DE PRUEBAS
+    def insertarSide():
+        pass #AQUI DEBEMOS PONER EL METODO YA HECHO EN EL ARCHIVO "DataBaseYGO.py"
+        
+
+    
+    def VisualizarSide():
+        pass #AQUI DEBEMOS PONER EL METODO YA HECHO EN EL ARCHIVO "DataBaseYGO.py"
+                #DEBE DE RETORNAR UNA LISTA PARA PASAR AL OBJETO DECK
+    
+   
+    #########################
+
+
+
+
+    #########################
+
+    
+    
