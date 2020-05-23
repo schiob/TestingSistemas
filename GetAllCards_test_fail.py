a7fc2fe07a1ddade22f0a1be851b08e65eca077b
unknown <onofreeduardos@gmail.com>
2020-05-22 20:19:40 -0500

    Prueba con Fallo TRAVIS

--- GetAllCards_test_fail.py
+++ GetAllCards_test_fail.py
@@ -7,7 +7,7 @@ class GetAllCards_test(unittest.TestCase):
     def setUp(self):
         print("Preparando el contexto...")
         self.opcion = 2
-        self.cards = getAllCards()
+        self.cards = 12348
         
     def test_file(self):
         entrada = self.opcion
