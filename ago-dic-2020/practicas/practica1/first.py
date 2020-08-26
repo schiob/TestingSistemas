56a0002479d28965bee688b65e422f6856fef464
Rephyroth <57422543+Rephyroth@users.noreply.github.com>
2020-08-26 17:18:29 -0500

    Add files via upload

---
+++ ago-dic-2020/practicas/practica1/first.py
@@ -0,0 +1,11 @@
+
+def suma(x,y):
+    res = x+y
+    
+    return res
+
+x = int(input("Introduce el primer Numero "))
+y = int(input("Introduce el segundo Numero "))
+
+print ("El valor de la suma es: ",suma(x,y))
+
