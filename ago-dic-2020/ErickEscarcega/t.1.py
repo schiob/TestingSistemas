a61330a04b8f528767008c9672136dff09f42ca6
ERORESRA <erick_escarcega@hotmail.com>
2020-08-26 16:58:09 -0500

    Practica 1

---
+++ ago-dic-2020/ErickEscarcega/t.1.py
@@ -0,0 +1,8 @@
+import sys
+print("give me numbarss:")
+o = sys.stdin.readline()
+t = sys.stdin.readline()
+print("Your result grrrrr:")
+sum = int(o) + int(t)
+res = str(sum)
+sys.stdout.write(res)
