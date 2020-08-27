3f1fde477c97458b367d7c62995c696330eec1ed
Arnoldo <juan.arnoldo.munoz@hotmail.com>
2020-08-27 12:23:50 -0500

    Commit de la primera practica

---
+++ ago-dic-2020/Juan Arnoldo Chavez Munoz/Practicas/Practica_1.py
@@ -0,0 +1,4 @@
+num1 = int(input("Introduce el primer número: "))
+num2 = int(input("Introduce el segundo número: "))
+suma = num1 + num2
+print("La suma de "+ str(num1) +" y "+str(num2)+ " es: " + str(suma))
