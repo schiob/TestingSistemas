c9f979d03113e24691670049165cd4f5d23d30c4
mariacastrotorres <mc146049@gmail.com>
2020-08-27 22:46:07 -0500

    practica 1

---
+++ ago-dic-2020/mariacastro/practicas/practica1/practica1.py
@@ -0,0 +1,9 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+x=int(input("indroduce el primer numero:"))
+y=int(input("introduce el segundo numero:"))
+
+suma=x+y
+print("la suma de los dos numeros es:", suma)
+
