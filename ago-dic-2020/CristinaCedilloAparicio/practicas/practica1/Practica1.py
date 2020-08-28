bcbb3cb0a5f96011a8745a65afbf0e1c64fa6047
cristy-ca98 <cristinacedilloapar@uadec.edu.mx>
2020-08-27 19:32:18 -0500

    Practica 1

---
+++ ago-dic-2020/CristinaCedilloAparicio/practicas/practica1/Practica1.py
@@ -0,0 +1,6 @@
+print("Esto es una suma de 2 números")
+print("---------------------------------------------")
+numero_uno = int(input("Por favor, escribe el primer número: "))
+numero_dos = int(input("Ahora, escribe el segundo número: "))
+resultado = numero_uno + numero_dos
+print("El resultado de ", numero_uno, " + " , numero_dos , " = " , resultado)
