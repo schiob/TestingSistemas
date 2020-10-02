a43fee3452119cc9e4aaf65edffa222bff345490
Rephyroth <57422543+Rephyroth@users.noreply.github.com>
2020-10-02 18:29:06 -0500

    Update test_primer_parcial.py

--- ago-dic-2020/primer_parcial/test_primer_parcial.py
+++ ago-dic-2020/primer_parcial/test_primer_parcial.py
@@ -3,50 +3,48 @@ import mock
 import primer_parcial
 ####### Test ######
 class Test_primer_parcial(unittest.TestCase):
-    def test_eqilatero_1(self):
+    def test_TestEquilatero(self):
         ######### Usando mock ###################
         with mock.patch("builtins.open", mock.mock_open(read_data="3 3 3"), create=True) as mock_file:
             result = primer_parcial.tri_from_file("path")
         mock_file.assert_called_once_with("path")
+          ######### Usando archivo ###################
+        resultf = primer_parcial.tri_from_file("equi2.txt")
+        
         self.assertEqual(result,"Equilátero")
+        self.assertEqual(resultf,"Equilátero")
 
-    def test_eqilatero_2(self):
-        ######### Usando archivo ###################
-        result = primer_parcial.tri_from_file("equi2.txt")
-        self.assertEqual(result,"Equilátero")
     
-    def test_no_triangulo_1(self):
+    def test_TestNoTri(self):
         ######### Usando mock ###################
         with mock.patch("builtins.open", mock.mock_open(read_data="0 0 0"), create=True) as mock_file:
             result = primer_parcial.tri_from_file("path")
         mock_file.assert_called_once_with("path")
-        self.assertEqual(result,"No triángulo")
-
-    def test_no_triangulo_2(self):
-        ######### Usando archivo ###################
-        result = primer_parcial.tri_from_file("notri2.txt")
-        self.assertEqual(result,"No triángulo")
-
-    def test_no_triangulo_3(self):
+         ######### Usando archivo ###################
+        resultf = primer_parcial.tri_from_file("notri2.txt")
         ######### Usando mock ###################
         with mock.patch("builtins.open", mock.mock_open(read_data="1 2 3"), create=True) as mock_file:
-            result = primer_parcial.tri_from_file("path")
+            resulta = primer_parcial.tri_from_file("path")
         mock_file.assert_called_once_with("path")
+        
+        self.assertEqual(resulta,"No triángulo")
         self.assertEqual(result,"No triángulo")
+        self.assertEqual(resultf,"No triángulo")
 
-    def test_isoceles_1(self):
+       
+    def test_TestIsoceles(self):
         ######### Usando archivo ###################
-        result = primer_parcial.tri_from_file("iso1.txt")
-        self.assertEqual(result,"Isóceles")
-
-    def test_isoceles_2(self):
+        resulta = primer_parcial.tri_from_file("iso1.txt")
         ######### Usando mock ###################
         with mock.patch("builtins.open", mock.mock_open(read_data="4 5 4"), create=True) as mock_file:
             result = primer_parcial.tri_from_file("path")
         mock_file.assert_called_once_with("path")
+        
+        self.assertEqual(resulta,"Isóceles")
         self.assertEqual(result,"Isóceles")
 
-    def test_escaleno_1(self):
+        
+    def test_TestEscaleno(self):
         ######### Usando mock ###################
         with mock.patch("builtins.open", mock.mock_open(read_data="8 6 5"), create=True) as mock_file:
             result = primer_parcial.tri_from_file("path")
@@ -57,5 +55,7 @@ class Test_primer_parcial(unittest.TestCase):
         
 if __name__ == '__main__':
     unittest.main()
+    
+    
     
     
