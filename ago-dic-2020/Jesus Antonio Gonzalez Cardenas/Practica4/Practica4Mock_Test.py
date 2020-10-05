19715590118b442f7d008bd34a2eeca1a503de3d
Jesús González <jesus_glezz229@hotmail.com>
2020-10-04 23:03:51 -0500

    Commit de correción de problemas de test de mock para practica 4

--- ago-dic-2020/Jesus Antonio Gonzalez Cardenas/Practica4/Practica4Mock_Test.py
+++ ago-dic-2020/Jesus Antonio Gonzalez Cardenas/Practica4/Practica4Mock_Test.py
@@ -1,11 +1,16 @@
-from Practica4 import getAverages
 import unittest
-import unittest.mock
-from unittest.mock import patch,MagicMock
+import Practica4
+import mock
 
 class csvTest(unittest.TestCase):
-    def test1(self):
-        mock_csv = MagicMock()
-        testdict = {'hotmail.com': 81.33, 'protonmail.com': 88.33}
+    doml = ['hotmail.com', 'protonmail.com']
+    
+    @mock.patch('Practica4.getDomains', return_value = doml)
+    def test_acces_file(self, mock_get):
+        data_expected = {'hotmail.com': 81.33, 'protonmail.com': 88.33}
+        self.assertEqual(Practica4.getAverages("Practica4TData.csv"),data_expected)
+       
 
 
+if __name__ == '__main__':
+    unittest.main()
