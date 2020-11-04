828549911d958c1e2eff3faf1e6e37292dd69cd6
D. Rodriguez Martinez <d.rdz.mtz314@gmail.com>
2020-11-04 15:34:45 -0600

    aniado folder Regression

---
+++ ago-dic-2020/DRodriguezMartinez/Presentacion System Testing/Regression/primer_parcial_test.py
@@ -0,0 +1,61 @@
+from  unittest import mock, TestCase
+from regression import Tri_from_file
+
+class Test_regression  (TestCase):
+    mocks = {"equi1": "3 3 3",
+             "equi2": "5 5 5",
+             "notri1": "0 0 0",
+             "notri2": "0 5 7",
+             "notri3": "1 2 3",
+             "iso1": "10 7 7",
+             "iso2": "4 5 4",
+             "esca1": "8 6 5"
+            }
+
+#############
+    def testEquilatero(self):
+        esperado = "Equilátero"
+        
+        with mock.patch('regression.open') as mock_open:
+            
+            for m in [v for i,v in self.mocks.items() if "equi" in i]:           
+                mock_open.return_value.__enter__.return_value.read.return_value = m
+                real = Tri_from_file("mock")
+
+                assert esperado == real
+
+#############
+    def testNoTri(self):
+        esperado = "No triángulo"
+
+        with mock.patch('regression.open') as mock_open:
+            
+            for m in [v for i,v in self.mocks.items() if "notri" in i]:                  
+                mock_open.return_value.__enter__.return_value.read.return_value = m
+                real = Tri_from_file("mock")
+
+                assert esperado == real
+
+#############
+    def testIsoceles(self):
+        esperado = "Isóceles"
+
+        with mock.patch('regression.open') as mock_open:
+            
+            for m in [v for i,v in self.mocks.items() if "iso" in i]:                  
+                mock_open.return_value.__enter__.return_value.read.return_value = m
+                real = Tri_from_file("mock")
+
+                assert esperado == real
+
+#############
+    def testEscaleno(self):
+        esperado = "Escaleno"
+
+        with mock.patch('regression.open') as mock_open:
+            
+            for m in [v for i,v in self.mocks.items() if "esca" in i]:                  
+                mock_open.return_value.__enter__.return_value.read.return_value = m
+                real = Tri_from_file("mock")
+
+                assert esperado == real
---
+++ ago-dic-2020/DRodriguezMartinez/Presentacion System Testing/Regression/regression.py
@@ -0,0 +1,24 @@
+def Tri_from_file(filepath: str) -> str:
+    
+    def triangleInequality(a,b,c):
+
+        if a + b > c and b + c > a and a + c > b:
+            return True
+        else:
+            return False   
+     ######
+
+    with open(filepath) as reader:
+        content = reader.read()
+
+    a,b,c = [int(i) for i in content.split()]
+    if triangleInequality(a,b,c):
+        if a == b == c: return "Equilátero"
+        if a == b or a == c or b == c: return "Isóceles"
+        if (a != b != c): return "Escaleno"
+
+    return "No triángulo"
+
+###
+if __name__ == "__main__":
+    print(Tri_from_file("triangulo.txt"))
