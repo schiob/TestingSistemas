9fe229ce8685b995e7eac5dcc05d26a2084a218c
developer-jesus-github <developerjesus@hotmail.com>
2020-11-13 19:38:46 -0600

    Ahora si, solo deje los archivos necesarios

--- ago-dic-2020/jesus-angel-rodriguez-martinez/practica-1/practica-1.py
+++
@@ -1,6 +0,0 @@
-#   Practica 1
-#   Escribe un pequeño programa que tome 2 números de STDIN y muestre el resultado de su suma.
-
-#   No validé nada, me imagino que se usará de prueba para el testing.
-
-print(int(input('Ingresa el número 1 \n'))+int(input('Ingresa el número 2 \n')))
