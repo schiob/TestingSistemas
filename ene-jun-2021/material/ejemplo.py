8630da42b20d939b89cc4b5bac889b4c78fc00bb
Santiago Chio <schiob4@gmail.com>
2021-02-12 18:34:25 -0600

    practica 3

---
+++ ene-jun-2021/material/ejemplo.py
@@ -0,0 +1,14 @@
+def tipoTriangulo(a: int, b: int, c: int):
+    '''Me regresa el tipo de triángulo que es, iso, equi, etc, etc, etc.'''
+    pass
+
+'''
+<(5, 5, 5), "equilátero">
+<(1000000, 1000000, 1000000), "equilátero">
+<(0, 0, 0), "no">
+<(4, 3, 0), "no">
+<(-4, 3, 5), "no">
+<(5, 3, 5), "iso">
+<(5, 3, 6), "esc">
+<(2, 2, 4), "no">
+'''
---
+++ ene-jun-2021/practicas/practica3/README.md
@@ -0,0 +1,4 @@
+# Práctica 3
+Toma los ejercicios de la práctica 1 y 2 y realiza pruebas unitarias con `unittest` o `pytest` con diferentes casos de prueba para cada uno de los ejercicios.
+
+Si es necesario hacer modificaciones en las unidades a probar, dale. :)
