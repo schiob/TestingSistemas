556894efc19cc5672709cf41ef160151178b36ac
Pablo VR <pavara_2010@hotmail.com>
2021-08-27 16:11:26 -0500

    Se agrego linea hola

--- ago-dic-2021/Pablo Valera Rangel/practica1.py
+++ ago-dic-2021/Pablo Valera Rangel/practica1.py
@@ -4,7 +4,7 @@ a = random.randrange(20)
 b = random.randrange(20)
 
 resuma = a + b
-print ('Numero a: ' , a)
-print ('Numero b: ' , b)
-print('La suma de los numeros es: ' , resuma)
-
+print('Numero a: ', a)
+print('Numero b: ', b)
+print('La suma de los numeros es: ', resuma)
+print('Hola?')
