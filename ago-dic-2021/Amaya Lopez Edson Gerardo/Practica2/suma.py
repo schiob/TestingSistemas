79b2bab7b8e1f84252d50882d8742d0cc6aa1352
Edson Amaya <edson_amaya99@live.com>
2021-09-08 19:20:10 -0500

    TestingSistemas {Master} Push: {Practica 2}

---
+++ ago-dic-2021/Amaya Lopez Edson Gerardo/Practica2/suma.py
@@ -0,0 +1,6 @@
+
+
+def suma(a, b):
+    return a + b
+
+
---
+++ ago-dic-2021/Amaya Lopez Edson Gerardo/test1/contar.py
@@ -0,0 +1,12 @@
+def contar_vocales(linea):  
+    cont =0
+    for letra in linea:
+        if letra in "aeiou":
+             cont +=1
+    
+    return cont
+
+
+if __name__ == '__main__':
+    linea = input()
+    print(contar_vocales())
