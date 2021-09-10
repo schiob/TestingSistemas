36655412206e9221d95d8aa45269910daebc3979
CristianF50 <mrcris.fdz@gmail.com>
2021-09-10 02:29:19 -0500

    Se agrega la practica 2

---
+++ ago-dic-2021/fernandez-salinas-cristian-alejandro/Practica2.py
@@ -0,0 +1,14 @@
+import unittest
+
+def suma (n1,n2):
+    sum = int(n1) + int(n2)
+    print(sum)
+    return sum
+
+class TestSuma(unittest.TestCase):
+    def test_suma(self):
+        actual = suma(1,2)
+        expected = 3
+        self.assertEqual(actual,expected)
+
+#suma(1,2)
