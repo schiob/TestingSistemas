d81e86a3336aa77d96cd5c93c4b3a328b252cecd
Erick <delabra32@gmail.com>
2021-09-22 19:06:22 -0500

    Examen parcial 1 (AHORA SI) le puse mas pruebas

--- ago-dic-2021/Diaz Delabra Erick/Parcial 1/taquitos_test.py
+++ ago-dic-2021/Diaz Delabra Erick/Parcial 1/taquitos_test.py
@@ -4,9 +4,35 @@ from taquitos import taquitosClase
 
 class testTacos(unittest.TestCase):
     def test1(self):
+        '''ejemplo del prof'''
         lista = ["cachete", "lengua", "cachete", "tripitas", "machito", "machito", "machito", "cachete", "lengua"]
         entrada = taquitosClase(lista)
         self.assertEqual(entrada, 110)
+
+    def test2(self):
+        '''orden de lengua'''
+        lista = ["lengua", "lengua", "lengua", "lengua", "lengua"]
+        entrada = taquitosClase(lista)
+        self.assertEqual(entrada, 50)
+
+    def test3(self):
+        '''sin hambre'''
+        lista = ["pastor"]
+        entrada = taquitosClase(lista)
+        self.assertEqual(entrada, 15)
+
+    def test4(self):
+        '''con la novia (dos ordenes)'''
+        lista = ["pastor", "pastor", "cachete", "tripitas", "machito", "machito", "machito", "cachete", "lengua", "pastor", "tripitas"]
+        entrada = taquitosClase(lista)
+        self.assertEqual(entrada, 141)
+
+    def test5(self):
+        '''uno de cada uno'''
+        lista = ["cachete", "lengua", "tripitas", "pastor", "machito"]
+        entrada = taquitosClase(lista)
+        self.assertEqual(entrada, 61)
+    
 
 if __name__ == '__main__':
     unittest.main()
