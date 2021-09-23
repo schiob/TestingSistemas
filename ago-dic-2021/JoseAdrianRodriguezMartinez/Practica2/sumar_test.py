9927b0d5e566b7ae873ef2c5ef6b0a286fe3a348
jarmarj <jarmarj_first@hotmail.com>
2021-09-22 19:01:06 -0500

    modifique practica 2

--- ago-dic-2021/JoseAdrianRodriguezMartinez/Practica2/sumar_test.py
+++ ago-dic-2021/JoseAdrianRodriguezMartinez/Practica2/sumar_test.py
@@ -3,19 +3,19 @@ from sumar import calculo
 
 class TestSumar(unittest.TestCase):
 
-    def sumar_positivos(self):
+    def test_sumar_positivos(self):
         calculo_real = calculo(10, 10)
         self.assertEqual(calculo_real, 20)
 
-    def sumar_multiplicados(self):
+    def test_sumar_multiplicados(self):
         calculo_real = calculo(10*10, 50)
         self.assertEqual(calculo_real, 150)
     
-    def sumar_divididos(self):
+    def test_sumar_divididos(self):
         calculo_real = calculo(10/5, 3)
         self.assertEqual(calculo_real, 5)
     
-    def sumar_negativos(self):
+    def test_sumar_negativos(self):
         calculo_real = calculo(-50, -20)
         self.assertEqual(calculo_real, -70)
 
