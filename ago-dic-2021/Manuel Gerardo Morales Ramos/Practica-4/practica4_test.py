ddcf6871b917a2d1d399afefa520fed652f99f33
Gerardo M. R <gerardo_9978@outlook.com>
2021-10-01 19:37:59 -0500

    refact(practica-4): test cases list added to iterate them and avoid test many test_[...] functions

--- ago-dic-2021/Manuel Gerardo Morales Ramos/Practica-4/practica4_test.py
+++ ago-dic-2021/Manuel Gerardo Morales Ramos/Practica-4/practica4_test.py
@@ -1,29 +1,49 @@
 import unittest
-from  unittest.mock import mock_open, patch
+from  unittest import mock
 from practica4 import calc_prom
 
+test_cases = [
+            {
+                'name': 'Case 1 ok',
+                'input': '10\n20\n30',
+                'expected_output': 60,
+                'expected_raise': False,
+            },
+            {
+                'name': 'Case 2 ok',
+                'input': '10\n20\n30\n10',
+                'expected_output': 70,
+                'expected_raise': False,
+            },
+             {
+                'name': 'Case 3 ok',
+                'input': '10',
+                'expected_output': 10,
+                'expected_raise': False,
+            },
+             {
+                'name': 'Case 4 ok',
+                'input': '',
+                'expected_output': 0,
+                'expected_raise': False,
+            },
+             {
+                'name': 'Case 5 error',
+                'input': 'refe',
+                'expected_output': 0,
+                'expected_raise': True,
+            },
+    ]
 class TestPractica4(unittest.TestCase):
-    @patch('builtins.open', mock_open(read_data='10\n20\n30'))
-    def test_open_file_1(self):
-        string_read = calc_prom()
-        self.assertEqual(string_read, 60)
-        open.assert_called_with('archivito.txt', 'r')
-
-    @patch('builtins.open', mock_open(read_data='10\n20\n30\n10'))
-    def test_open_file_2(self):
-        string_read = calc_prom()
-        self.assertEqual(string_read, 70)
-        open.assert_called_with('archivito.txt', 'r')
-
-    @patch('builtins.open', mock_open(read_data=''))
-    def test_open_file_3(self):
-        string_read = calc_prom()
-        self.assertEqual(string_read, 0)
-        open.assert_called_with('archivito.txt', 'r')
-    
-    @patch('builtins.open', mock_open(read_data='refe'))
-    def test_open_file_4(self):
-        self.assertRaises(Exception, calc_prom, 'refe')
+    def test_archivito(self):
+        for test in test_cases:
+            if test["expected_raise"]:
+                self.assertRaises(Exception, calc_prom, test["input"])
+            else:
+                mock_open = mock.mock_open(read_data=test['input'])
+                with mock.patch('builtins.open', mock_open):
+                    result = calc_prom()
+                self.assertEqual(test['expected_output'], result)
 
 if __name__ == '__main__':
     unittest.main()
