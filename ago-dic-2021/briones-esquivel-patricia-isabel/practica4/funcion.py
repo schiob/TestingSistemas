bdf3a9d957666a6d4e20c7432dc8ec3ac6ba05a7
ackerman507 <ackerman507@hotmail.com>
2021-10-07 01:18:55 -0500

    TestingSistemas Master Push: Práctica 4
    Modificando práctica 4: Mock para sustituir un file (Ya funciona correctamente).

--- ago-dic-2021/briones-esquivel-patricia-isabel/practica4/funcion.py
+++ ago-dic-2021/briones-esquivel-patricia-isabel/practica4/funcion.py
@@ -1,18 +1,23 @@
 class leer_archivo (): 
-    def leer(self) -> str: 
-        with open("archivito.txt", "r") as f:
-            return f.read()
+    def leer(self) -> list: 
+        datos = []
+        with open("archivito.txt") as fname:
+            lineas = fname.readlines()
+            for linea in lineas:
+                datos.append(linea.strip('\n'))
+        return datos
 
 def calc_prom(entrada):
     total = 0
     n = 0
     for linea in entrada.leer():
         #if linea[0].isnumeric():
-        if linea.split():
-            total += int(linea)
-            n += 1
+        #if linea.split():
+        total += int(linea)
+        n += 1
     return total / n
 
 if __name__ == "__main__": 
     archivito = leer_archivo() 
     print(calc_prom (archivito))
+    
--- ago-dic-2021/briones-esquivel-patricia-isabel/practica4/funcion_test.py
+++ ago-dic-2021/briones-esquivel-patricia-isabel/practica4/funcion_test.py
@@ -5,13 +5,14 @@ class NuevoMock ():
     def __init__(self, return_value): 
         self.return_value = return_value
 
-    def leer(self) -> str: 
-        return self.return_value
+    def leer(self) -> list: 
+        datos = self.return_value.split('\n')
+        return datos
 
 class TestMock(unittest.TestCase):         
     def test_mock(self):
-        mi_mock = NuevoMock("1\n1") 
-        self.assertEqual(calc_prom(mi_mock), 1)
+        mi_mock = NuevoMock("10\n15") 
+        self.assertEqual(calc_prom(mi_mock), 12.5)
         
 if __name__ == "__main__":
     unittest.main()
