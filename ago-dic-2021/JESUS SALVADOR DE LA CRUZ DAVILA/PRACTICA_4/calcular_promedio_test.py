696816c2366507472566b6fc01cb4dc969077908
Jisus1840 <delacruzjesus083@gmail.com>
2021-10-10 10:58:36 -0500

    Correccion de la practica 4, se movio cuando subi el resumen de la conferencia

--- ago-dic-2021/JESUS SALVADOR DE LA CRUZ DAVILA/PRACTICA_4/calcular_promedio_test.py
+++ ago-dic-2021/JESUS SALVADOR DE LA CRUZ DAVILA/PRACTICA_4/calcular_promedio_test.py
@@ -7,8 +7,9 @@ from calcular_promedio import *
 
 
 class PromedioTest(TestCase):
-    @patch('__main__.open')
+    @patch('__main__.open', create=True)
     def test_archivito(self, mock_open):
+        mock_open.return_value = 15
         salida_esperada = 3
         self.assertEqual(calc_prom(), salida_esperada)
 
