31b2d14cbdeb30f62abffdb250710bda8b2b1148
Crisolino <mrcris.fdz@gmail.com>
2021-11-24 23:04:51 -0600

    Practicas Pendientes

--- ago-dic-2021/fernandez-salinas-cristian-alejandro/Parcial2Pt2.py
+++ ago-dic-2021/fernandez-salinas-cristian-alejandro/Parcial2Pt2.py
@@ -4,21 +4,21 @@ from PIL import Image, ImageDraw
 
 class Escritor(ABC):
     @abstractmethod
-    def escribir(self, usrtxt:str):
+    def escribir(self, usrtxt:str) -> str:
        pass
         
 
 class InsertarTexto(Escritor):
     
-    def enImagen(texto:str):
+    def enImagen(texto:str) -> str:
         texto = texto + "usamos python para esto :)"
         imagenAUsar = Image.open("imagen.jpg")
         imagenEditable = ImageDraw.Draw(imagenAUsar)
         imagenEditable.text((15,15), texto, (255,255,255))
         imagenAUsar.save("resultado.jpg")
-
+        return texto
 
 if __name__ == "__main__":
     textoAUsar = input('Escribe palabras a insertar en la imagen')
-    InsertarTexto.enImagen(textoAUsar)
+    print(InsertarTexto.enImagen(textoAUsar))
  
--- ago-dic-2021/fernandez-salinas-cristian-alejandro/funcion.py
+++ ago-dic-2021/fernandez-salinas-cristian-alejandro/funcion.py
@@ -1,10 +1,15 @@
 def calc_prom():
     file = open("archivito.txt", "r")
     total = 0
+    cantidad = 0
     for linea in file:
         total += int(linea)
+        cantidad += 1
+
+    if cantidad == 0:
+        return 'Porfavor ingresa datos'
     
-    return total
+    return round(total / cantidad, 2)
 
 if __name__ == "__main__":
     print(calc_prom())
