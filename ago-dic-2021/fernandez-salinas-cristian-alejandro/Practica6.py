b5d299ce19731214376a577b6fee96094c4f7636
Crisolino <mrcris.fdz@gmail.com>
2021-12-01 02:56:52 -0600

    Practica 6 y 7

---
+++ ago-dic-2021/fernandez-salinas-cristian-alejandro/Practica6.py
@@ -0,0 +1,50 @@
+user_priority = {
+    'Tom':    4,
+    'Susana': 2,
+    'Andres':10,
+    'Pepe':   3,
+    'Luis':   2
+}
+conductores = {
+    'Juan' :  3,
+    'Jesus': 2,
+    'Maria': 3,
+    'Toño' : 1
+}
+viajes= {}
+def siguiente_usuario():
+    next_user = min(user_priority,key=user_priority.get)
+    user_priority.pop(next_user)
+    return next_user
+
+def extraer_conductores():
+    return conductores
+
+def calcular_tarifa(conductor):
+    antiguedad = conductores.get(conductor)
+    tarifa = 20 + (10* antiguedad)
+    return tarifa
+
+def viajes_disponibles():
+    y=1
+    for x in extraer_conductores():
+        tarifa= calcular_tarifa(x)
+        viajes[y] =tarifa
+        y +=1
+    return viajes
+
+contador = len(user_priority)
+user_viaje = {}
+def principal():
+    viaje = viajes_disponibles()
+    for x in range(contador-1):
+        usuario = siguiente_usuario()
+        id_viaje = min(viaje,key=viaje.get)
+        user_viaje[id_viaje] =usuario
+        del viaje[id_viaje]
+    return user_viaje
+
+print('='.center(50,"="))
+print(calcular_tarifa('Juan'))
+if __name__ == "__main__":
+    principal() 
